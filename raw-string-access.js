ca901fd7a1781f344aeaf553c38c3c86a41ce85e
Sıtkı <53573445+stkalagoz@users.noreply.github.com>
2020-04-05 03:00:30 +0300

    raw-string-access file added

---
+++ raw-string-access.js
@@ -0,0 +1,11 @@
+let str1 =`C:\Development\profile\aboutme.html`;
+console.log(str1);
+// output => C:Developmentprofileaboutme.html
+
+let str2 = String.raw`C:\Development\profile\aboutme.html`;
+console.log(str2);
+// output => C:\Development\profile\aboutme.html
+
+
+
+
